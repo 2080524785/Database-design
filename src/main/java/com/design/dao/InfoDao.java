30a1d1cf65bee4c96c82ba37706e6e26eb1121d7
2080524785 <2080524785@qq.com>
2022-12-20 22:40:31 +0800

    12月20日 修改sql语句，对齐变量

--- src/main/java/com/design/dao/InfoDao.java
+++ src/main/java/com/design/dao/InfoDao.java
@@ -20,13 +20,13 @@ public interface InfoDao {
     @Select("SELECT t.date,coalesce(u.number,0) 'number' from(SELECT subdate(CURRENT_DATE, numlist.id) AS 'date' FROM (SELECT DISTINCT x.i + y.i * 10 + z.i * 100 AS id FROM num x,num y,num z ORDER BY id) AS numlist WHERE subdate(CURRENT_DATE, numlist.id) > date_sub(CURRENT_DATE,interval 1 year)) t LEFT JOIN (SELECT DATE(Borrow.borrow_time)as date,count(DISTINCT sno) number FROM Borrow WHERE sno=#{sno} GROUP BY DATE(Borrow.borrow_time)) u on t.date = u.date ORDER BY t.date")
     public List<Info> getOneStuDataBorrow(String sno);
 
-    @Select("SELECT  Book_info.name,count(*) num from Borrow,Book_info where DATE(borrow_time)>DATE_SUB(CURDATE(), INTERVAL 1 YEAR) and Book_info.id=Borrow.id GROUP BY Book_info.name,Book_info.pub,Book_info.time  ORDER BY num DESC limit 5")
+    @Select("SELECT  Book_info.name,count(*) number from Borrow,Book_info where DATE(borrow_time)>DATE_SUB(CURDATE(), INTERVAL 1 YEAR) and Book_info.id=Borrow.id GROUP BY Book_info.name,Book_info.pub,Book_info.time  ORDER BY num DESC limit 5")
     public List<Info> getYearRankBookBorrow();
-    @Select("SELECT  Book_info.name,count(*) num from Borrow,Book_info where DATE(borrow_time)>DATE_SUB(CURDATE(), INTERVAL 1 MONTH) and Book_info.id=Borrow.id GROUP BY Book_info.name,Book_info.pub,Book_info.time  ORDER BY num DESC limit 5")
+    @Select("SELECT  Book_info.name,count(*) number from Borrow,Book_info where DATE(borrow_time)>DATE_SUB(CURDATE(), INTERVAL 1 MONTH) and Book_info.id=Borrow.id GROUP BY Book_info.name,Book_info.pub,Book_info.time  ORDER BY num DESC limit 5")
     public List<Info> getMonthRankBookBorrow();
-    @Select("SELECT  Book_info.name,count(*) num from Borrow,Book_info where DATE(borrow_time)>DATE_SUB(CURDATE(), INTERVAL 1 WEEK) and Book_info.id=Borrow.id GROUP BY Book_info.name,Book_info.pub,Book_info.time  ORDER BY num DESC limit 5")
+    @Select("SELECT  Book_info.name,count(*) number from Borrow,Book_info where DATE(borrow_time)>DATE_SUB(CURDATE(), INTERVAL 1 WEEK) and Book_info.id=Borrow.id GROUP BY Book_info.name,Book_info.pub,Book_info.time  ORDER BY num DESC limit 5")
     public List<Info> getWeekRankBookBorrow();
-    @Select("SELECT  Book_info.name,count(*) num from Borrow,Book_info where DATE(borrow_time)=CURRENT_DATE and Book_info.id=Borrow.id GROUP BY Book_info.name,Book_info.pub,Book_info.time  ORDER BY num DESC limit 5")
+    @Select("SELECT  Book_info.name,count(*) number from Borrow,Book_info where DATE(borrow_time)=CURRENT_DATE and Book_info.id=Borrow.id GROUP BY Book_info.name,Book_info.pub,Book_info.time  ORDER BY num DESC limit 5")
     public List<Info> getDayRankBookBorrow();
 
     @Select("SELECT count(sno) from Stu_info ")
